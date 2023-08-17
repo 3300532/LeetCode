9805466fd0ff5df3f7bd423d6307efc8ace84825
Aidos <103981137+aidos-dev@users.noreply.github.com>
2023-08-17 15:20:25 +0600

    Time: 8 ms (66.09%), Space: 6.7 MB (79.31%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.go
@@ -0,0 +1,37 @@
+func removeDuplicates(input string) string {
+
+	buf := make([]byte, len(input))
+
+	checkPoint := 0
+	recordPoint := 0
+
+	for i := 0; i < len(input); i++ {
+
+		if buf[0] == 0 && i < len(input)-1 {
+			buf[0] = input[i]
+			recordPoint++
+			i++
+		}
+
+		if input[i] != buf[checkPoint] {
+			buf[recordPoint] = input[i]
+			checkPoint++
+			recordPoint++
+		} else {
+			buf[checkPoint] = 0
+
+			if checkPoint != 0 {
+				checkPoint--
+				recordPoint--
+			} else {
+				recordPoint = 0
+			}
+
+		}
+
+	}
+
+	buf = buf[:recordPoint]
+
+	return string(buf)
+}
