2c857a97df3d94b028075617458c90fa5e8d2258
Aidos <103981137+aidos-dev@users.noreply.github.com>
2023-08-17 18:33:42 +0600

    Time: 8 ms (59.40%), Space: 4.3 MB (13.10%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.go
@@ -0,0 +1,16 @@
+func twoSum(nums []int, target int) []int {
+	hashSet := make(map[int]int)
+
+	for i, el := range nums {
+		difference := target - el
+
+		_, exists := hashSet[difference]
+
+		if exists {
+			return []int{i, hashSet[difference]}
+		}
+		hashSet[el] = i
+	}
+
+	return []int{}
+}
