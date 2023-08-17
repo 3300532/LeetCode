aa466c48cece94b10f021382d56fc2121dd681d3
Aidos <103981137+aidos-dev@users.noreply.github.com>
2023-08-17 15:14:12 +0600

    Time: 91 ms (87.60%), Space: 9.9 MB (47.02%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -0,0 +1,14 @@
+func containsDuplicate(nums []int) bool {
+    	hashSet := make(map[int]struct{})
+
+	for _, el := range nums {
+		_, exists := hashSet[el]
+		if exists {
+			return true
+		}
+
+		hashSet[el] = struct{}{}
+	}
+
+	return false
+}
